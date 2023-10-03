955002ffaceac132f417db7ecefd1da6bceb9d7c
emiralis <146878687+emiralis@users.noreply.github.com>
2023-10-03 21:58:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import random
+import time
 
 st.set_page_config(
     page_title="Matematik Game",
@@ -9,33 +10,25 @@ st.set_page_config(
 )
 st.title("Matematik Çalışma 5. Sınıf ➗😎")
 
-
-
-
-import random
-import time
-
 x = random.randint(11, 100)
 y = random.randint(1, 10)
-print("Bölünen sayısı =", x)
-print("Bölen sayısı =", y)
+st.write("Bölünen sayısı =", x)
+st.write("Bölen sayısı =", y)
 dogru_bolum = x // y
 dogru_kalan = x % y
 
+start_time = time.time()  # Başlangıç zamanını al
+
 while True:
-    start_time = time.time()  # Başlangıç zamanını al
-
     bolum = st.number_input('Bölüm')
-
-
-
+  
     if bolum == dogru_bolum:
         st.write("Doğru bölüm cevabı!")
         st.write("Kalan sonucunu yaz")
         kalan = st.number_input('Kalan')
-        end_time = time.time()  # Yeni bitiş zamanını al
-        elapsed_time = end_time - start_time  # Yeni geçen süreyi hesapla
-
+        end_time = time.time()  # Bitiş zamanını al
+        elapsed_time = end_time - start_time  # Geçen süreyi hesapla
+  
         if kalan == dogru_kalan:
             st.info("Doğru kalan cevabı!")
             st.info(f"Toplam süre: {elapsed_time:.0f} saniye")  # Süreyi yazdır
